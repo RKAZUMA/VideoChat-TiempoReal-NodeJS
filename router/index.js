a2c9887ed5ba28dab3dd7936b41cc1707a4f98b3
ivanbtrujillo <ivanbtrujillo@gmail.com>
2015-08-18 10:22:02 +0100

    Añadiendo course para manejar las rutas

--- router/index.js
+++ router/index.js
@@ -1,7 +1,10 @@
 'use strict'
 
 const st = require('st')
+const course = require('course')
 const path = require('path')
+
+const router = course();
 
 //Configuramos el modulo st para servir ficheros estaticos
 const mount = st({
@@ -15,8 +18,14 @@ function onRequest(req, res){
     if (err)
       return res.end(err.message)
 
-    res.statusCode = 404
-    res.end(`No se encuentra la ruta ${req.url}`)
+    router(req, res, function(err){
+      if(err)
+        return fail(err, res)
+
+      res.statusCode = 404
+      res.end(`No se encuentra la ruta ${req.url}`)
+    })
+
 
   })
 }
