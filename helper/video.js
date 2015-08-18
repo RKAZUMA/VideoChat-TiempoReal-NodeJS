0cd45be46869258629834bf1eca1f929e3ddeca8
ivanbtrujillo <ivanbtrujillo@gmail.com>
2015-08-18 11:07:46 +0100

    Añadiendo Async para el manejo de callbacks en el proceso de decodificacion de imagenes y conversion de video

--- helper/video.js
+++ helper/video.js
@@ -1,12 +1,46 @@
 'use strict'
 const EventEmitter = require('events').EventEmitter
+const async = require('async')
 
 module.exports = function(images){
   let events = new EventEmitter()
 
-  setTimeout(function (){
-    events.emit('video', 'Esto será el video codificado')
-  })
+  //Ejecutamos las funciones en serie
+  async.series([
+    decodeImages,
+    createVideo,
+    encodeVideo,
+    cleanup,
+    convertFinished
+  ])
+
+
+  //Decodificamos las imagenes de base64 a buffer
+  function decodeImages(done){
+    done()
+  }
+
+  //Creamos el video a partir de las imagenes
+  function createVideo(done){
+    done()
+  }
+
+  //Codificamos el video
+  function encodeVideo(done){
+    done()
+  }
+
+  //Borramos los archivos temporales de las imagenes
+  function cleanup(done){
+    done()
+  }
+
+  //Cuando se termina la conversion
+  function convertFinished(err){
+    setTimeout(function(){
+      events.emit('video', 'Este sera el video codificado')
+    }, 100)
+  }
 
   return events
 }
