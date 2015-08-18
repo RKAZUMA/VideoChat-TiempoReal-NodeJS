76a04ed8326fe0fa9f641df4e08a6eab926ee80b
ivanbtrujillo <ivanbtrujillo@gmail.com>
2015-08-18 11:26:00 +0100

    Decodificando imagenes con data-uri-to-images y guardandolas en la carpeta tmp del sistema

--- helper/video.js
+++ helper/video.js
@@ -1,9 +1,21 @@
 'use strict'
 const EventEmitter = require('events').EventEmitter
 const async = require('async')
+const dataURIBuffer = require('data-uri-to-buffer')
+const uuid = require('uuid')
+const os = require('os')
+const fs = require('fs')
+const path = require('path')
 
 module.exports = function(images){
+
   let events = new EventEmitter()
+  //Contador
+  let count = 0
+  //Generamos un nombre unico
+  let baseName = uuid.v4()
+  //Directorio donde guardamos las imagenes en el directorio temp del SO
+  let tmpDir = os.tmpDir()
 
   //Ejecutamos las funciones en serie
   async.series([
@@ -17,7 +29,25 @@ module.exports = function(images){
 
   //Decodificamos las imagenes de base64 a buffer
   function decodeImages(done){
-    done()
+    //Decodificamos cada imagen
+    async.eachSeries(images, decodeImage, done)
+  }
+  //Convertimos la imagen a buffer
+  function decodeImage(image, done){
+    //Creamos el nombre del archivo
+    let fileName = `${baseName}-${count++}.jpg`
+    //Creamos el buffer a partir de la imagen
+    let buffer = dataURIBuffer(image)
+    //Creamos el fichero donde guardamos
+    let ws = fs.createWriteStream(path.join(tmpDir, fileName))
+
+    //Si hay un error llamamos al callback con error
+    ws.on('error', done)
+      //Si no hay error, continua
+      .end(buffer, done)
+
+      events.emit(`log`, `Convertiendo ${fileName}`)
+
   }
 
   //Creamos el video a partir de las imagenes
